3dcdd5e164572e92950dde57c4caa936f129228c
JurieZietsmanGithubPython <uberziets@gmail.com>
2020-05-22 10:55:48 +0200

    Test

--- Code/Functions.py
+++ Code/Functions.py
@@ -1,14 +1,31 @@
-def import_csv():
-    # global raw_data
-    # filePath, _ = QFileDialog.getOpenFileName('Open file', '/home', filter="csv(*.csv)")
-    # if filePath != "":
-    # raw_data = pd.read_csv(str(filePath))
-    # return raw_data
-    print('Hello')
-    print('Keen')
+from PyQt5.QtWidgets import QFileDialog
+import pandas as pd
 
-def hello():
-    return 0
+def file_open():
+    path = QFileDialog.getOpenFileName('Open CSV', )
 
-def print():
-    print('keen')
+def import_data():
+    """
+Imports csv or xls file from document explorer
+    """
+    filePath, _ = QFileDialog.getOpenFileName('Open file', '/home', filter="csv(*.csv)")
+    if filePath != "":
+        data = pd.read_csv(str(filePath))
+    return data
+
+
+def printer():
+    print('hel')
+
+
+def evaluate_forecast(y_pred, y_true, metric):
+    """
+Evaluate the performance of forecasted values against actual values according to some metric
+    :param y_pred: vector
+    :param y_true: vector
+    :param metric: string (mape, mse)
+    """
+    if metric == 'mape':
+        return 0
+    if metric == 'mse':
+        return 1
--- Code/Main_Window.py
+++ Code/Main_Window.py
@@ -3,17 +3,18 @@ import os
 import pandas as pd
 import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from Functions import import_csv
+from Functions import import_data, printer
+import warnings
+warnings.filterwarnings("ignore")
 
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        uic.loadUi("User Interface/main_window.ui", self)
+        uic.loadUi("C:/Users/Jurie/PycharmProjects/Thesis/User Interface/main_window.ui", self)
 
-        self.pushButton1.clicked.connect(import_csv)
-
+        self.import_csv_button.clicked.connect(printer)
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
