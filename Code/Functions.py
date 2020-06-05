3eb7159adc1b194230e344e4308073bcd46e0b96
JurieZietsmanGithubPython <uberziets@gmail.com>
2020-06-05 16:31:42 +0200

    Test

--- Code/Functions.py
+++ Code/Functions.py
@@ -1,31 +1,144 @@
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtGui import QCursor
+from PyQt5.QtWidgets import QFileDialog, QMessageBox, QTableWidgetItem
 import pandas as pd
-
-def file_open():
-    path = QFileDialog.getOpenFileName('Open CSV', )
-
-def import_data():
-    """
-Imports csv or xls file from document explorer
-    """
-    filePath, _ = QFileDialog.getOpenFileName('Open file', '/home', filter="csv(*.csv)")
-    if filePath != "":
-        data = pd.read_csv(str(filePath))
-    return data
+from PyQt5 import QtWidgets, uic, Qt
+import sys
+import ui_background
 
 
-def printer():
-    print('hel')
+class MainWindow(QtWidgets.QMainWindow):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi("C:/Users/Jurie/PycharmProjects/Thesis/User Interface/main_window.ui", self)
 
-def evaluate_forecast(y_pred, y_true, metric):
-    """
-Evaluate the performance of forecasted values against actual values according to some metric
-    :param y_pred: vector
-    :param y_true: vector
-    :param metric: string (mape, mse)
-    """
-    if metric == 'mape':
-        return 0
-    if metric == 'mse':
-        return 1
+    ##############################################################
+    # Functions for testing
+    ##############################################################
+    def change_text(self):
+        self.label_3.setText('lekker')
+
+    # def test(self, df):
+    #     print(df)
+    #     print(type(df))
+
+    def printer(self, dataframe):
+        print(dataframe)
+
+    ##############################################################
+    # Functions for homepage, navigation and closing application
+    ##############################################################
+    def on_startButton_clicked(self):
+        self.stack.setCurrentIndex(1)
+
+    def nextStack(self):
+        index = self.stack.currentIndex()
+        index = index + 1
+        self.stack.setCurrentIndex(index)
+
+    def close_application(self):
+        choice = QMessageBox.question(self, 'Close application', "Quit the application?",
+                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if choice == QMessageBox.Yes:
+            sys.exit()
+        else:
+            pass
+
+    ##############################################################
+    # Functions for data processing
+    ##############################################################
+
+    def import_csv(self, button):
+        """
+    Reads csv file from document explorer
+        """
+        filePath, _ = QFileDialog.getOpenFileName(self, 'Choose a file', '\Home', filter="csv(*.csv)")
+        if filePath != "":
+            self.QGuiApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
+            data = pd.read_csv(str(filePath))
+
+        if button == "historicSales":
+            data['POSTING DATE'] = pd.to_datetime(data['POSTING DATE'].astype(str), format='%Y%m%d')
+
+            column_remove = ['BRANCH', 'CUSTNO', 'DIN', 'MATL', 'ITEM NO', 'WEIGHT',
+                             'USER NAME', 'TYPEOFSALE', 'SLM', 'INVOICE NUMBER', 'NAME', '.',
+                             'THREAD', 'PL', 'COSTVAL', 'SALES VALUE', 'OVERBY', 'DESCRIPT',
+                             'INDUSTRY CODE', 'CONTRIBUTION%', 'GLGROUP', 'TRNSPCODE',
+                             ' ']  # These should be selected by the user
+            data.drop(column_remove, axis=1, inplace=True)
+            data = data.groupby(['POSTING DATE', 'STOCKNO']).sum()
+
+            self.historicSales = data
+
+        if button == "skuList":
+            data = data['STOCKNO'].tolist()
+            self.skuList = data
+
+        if button == "stockOnHand":
+            self.stockOnHand = data
+
+        if button == "bomList":
+            self.bomList = data
+
+        if button == "supplierOrigin":
+            self.supplierOrigin = data
+
+        if button == "warehouseInfo":
+            self.warehouseInfo = data
+
+        if button == "backlogData":
+            self.backlogData = data
+
+        self.QGuiApplication.restoreOverrideCursor()
+
+    def print_import(self):
+        print(self.historicSales)
+        print(type(self.historicSales))
+
+    ##############################################################
+    # Functions for forecasting
+    ##############################################################
+
+    def perform_forecast(self, skuList, historicSales):
+        """
+        Perform forecast for each SKU in skuList
+        """
+        pass
+        for sku in skuList:
+            skuHistoric = historicSales[historicSales.index.isin([sku], level='STOCKNO')]
+            skuHistoric.index = skuHistoric.index.droplevel('STOCKNO')
+            skuHistoric = skuHistoric.resample('M').sum()
+            print(skuHistoric)
+
+    def evaluate_forecast(self, y_pred, y_true, metric):
+        """
+        Evaluate the performance of forecasted values against actual values according to some metric
+        :param y_pred: vector
+        :param y_true: vector
+        :param metric: string (mape, mse)
+        """
+        pass
+        if metric == 'mape':
+            return 0
+        if metric == 'mse':
+            return 1
+
+    ##############################################################
+    # Functions for order analysis
+    ##############################################################
+
+    ##############################################################
+    # Functions for order list
+    ##############################################################
+
+    def createTable(self):
+        """
+        Populate table widgets to list suggested local and import orders
+        """
+        # Need to set number of rows
+        # Need to set number of columns
+        self.localOrderTableWidget.setItem(1, 0, QTableWidgetItem("0099"))
+        self.localOrderTableWidget.setItem(2, 0, QTableWidgetItem("0022"))
+
+        self.importOrderTableWidget.setItem(3, 0, QTableWidgetItem("0066"))
+        self.importOrderTableWidget.setItem(4, 0, QTableWidgetItem("0088"))
--- Code/Main_Window.py
+++ Code/Main_Window.py
@@ -2,21 +2,64 @@ import sys
 import os
 import pandas as pd
 import numpy as np
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from Functions import import_data, printer
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QHeaderView
+from Functions import MainWindow
 import warnings
+import qdarkgraystyle
+
 warnings.filterwarnings("ignore")
 
+class GuiApplication(MainWindow):
 
-class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        MainWindow.__init__(self)
+        self.setWindowIcon(QtGui.QIcon('Images for QT/Black_dot.png'))
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        uic.loadUi("C:/Users/Jurie/PycharmProjects/Thesis/User Interface/main_window.ui", self)
+        ############################################################
+        # Signals for startup, homepage, navigation and closing application
+        ############################################################
+        self.stack.setCurrentIndex(0)
+        self.orderListTabWidget.setCurrentIndex(0)
+        self.localOrderTableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.importOrderTableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-        self.import_csv_button.clicked.connect(printer)
+        self.startButton.clicked.connect(self.on_startButton_clicked)
+        self.forecastingButton.clicked.connect(self.nextStack)
+        self.orderanalysisButton.clicked.connect(self.nextStack)
+        self.orderlistButton.clicked.connect(self.nextStack)
+        self.quitButton.clicked.connect(self.close_application)
+
+        ############################################################
+        # Signals for data processing
+        ############################################################
+        self.historicSales = None
+        self.skuList = None
+        self.stockOnHand = None
+        self.bomList = None
+        self.supplierOrigin = None
+        self.warehouseInfo = None
+        self.backlogData = None
+
+        self.historicSalesButton.clicked.connect(lambda: self.import_csv("historicSales"))
+        self.skuListButton.clicked.connect(lambda: self.import_csv("skuList"))
+        self.stockOnHandButton.clicked.connect(lambda: self.import_csv("stockOnHand"))
+        self.bomListButton.clicked.connect(lambda: self.import_csv("bomList"))
+        self.supplierOriginButton.clicked.connect(lambda: self.import_csv("supplierOrigin"))
+        self.warehouseInfoButton.clicked.connect(lambda: self.import_csv("warehouseInfo"))
+        self.backlogDataButton.clicked.connect(lambda: self.import_csv("backlogData"))
+
+        self.testButton.clicked.connect(self.print_import)
+
+        self.orderlistButton.clicked.connect(self.createTable)
+
+    ############################################################
+    # Signals for forecasting
+    ############################################################
+
 
 app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
+window = GuiApplication()
+app.setStyleSheet(qdarkgraystyle.load_stylesheet())
 window.show()
 app.exec_()
